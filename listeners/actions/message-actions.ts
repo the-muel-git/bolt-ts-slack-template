903189a1e29ebca5a7d64f05ba379feceaf60004
the-muel-git <sam@howarth-ramsay.com>
2025-04-10 22:29:59 +0930

    Fix TypeScript typing issues in action handlers and view callback

--- listeners/actions/message-actions.ts
+++ listeners/actions/message-actions.ts
@@ -1,10 +1,11 @@
-import type { AllMiddlewareArgs, SlackActionMiddlewareArgs } from '@slack/bolt';
+import type { AllMiddlewareArgs, SlackActionMiddlewareArgs, BlockAction } from '@slack/bolt';
 
 // Simplified handlers to be fully implemented later
 export const addReactionHandler = async ({ 
   ack,
   logger,
-}: AllMiddlewareArgs & SlackActionMiddlewareArgs) => {
+  action,
+}: AllMiddlewareArgs & SlackActionMiddlewareArgs<BlockAction>) => {
   await ack();
   logger.info('Add reaction action triggered');
   // Implementation will be added once we fix TypeScript issues
@@ -13,7 +14,8 @@ export const addReactionHandler = async ({
 export const replyToMessageHandler = async ({ 
   ack,
   logger,
-}: AllMiddlewareArgs & SlackActionMiddlewareArgs) => {
+  action,
+}: AllMiddlewareArgs & SlackActionMiddlewareArgs<BlockAction>) => {
   await ack();
   logger.info('Reply to message action triggered');
   // Implementation will be added once we fix TypeScript issues
--- listeners/views/message-reply-view.ts
+++ listeners/views/message-reply-view.ts
@@ -1,11 +1,15 @@
-import type { AllMiddlewareArgs, SlackViewMiddlewareArgs } from '@slack/bolt';
+import type { AllMiddlewareArgs, SlackViewMiddlewareArgs, ViewSubmitAction } from '@slack/bolt';
 
 const messageReplyViewCallback = async ({
   ack,
+  view,
+  body,
   logger,
-}: AllMiddlewareArgs & SlackViewMiddlewareArgs) => {
+}: AllMiddlewareArgs & SlackViewMiddlewareArgs<ViewSubmitAction>) => {
   await ack();
   logger.info('Message reply view submitted');
+  // Access user via body.user instead of view.user if needed
+  // const userId = body.user.id;
   // Implementation will be added once we fix TypeScript issues
 };
 
